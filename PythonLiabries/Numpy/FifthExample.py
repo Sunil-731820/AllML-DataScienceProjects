e3abe275551af23b9ce158265151717029e9a0dc
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-01-02 00:13:01 +0530

    coded

--- PythonLiabries/Numpy/FifthExample.py
+++ PythonLiabries/Numpy/FifthExample.py
@@ -8,3 +8,23 @@ print(arr)
 arr1 = np.arange(10,100,5,int)
 print("The Arr1 is ")
 print(arr1)
+
+# The use Of the linSpace() in The Numpy Array
+arr = np.linspace(10,20,5,endpoint=False,retstep=True)
+print("The LinSpace Array ")
+print(arr)
+
+# The use of The LogSpace() In Numpy Array
+arr = np.logspace(10,20,num=15,endpoint=True)
+print("The Numpy Array is ")
+print(arr)
+print("The Length of the Numpy Array is ")
+print((arr.size))
+
+#Again The use of The logspace()
+arr = np.logspace(10,20,num=5,base=2,endpoint=True)
+print("The Numpy array is ")
+print(arr)
+
+
+
---
+++ PythonLiabries/Numpy/SixthExample.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.array([1,2,3,4,5,6,7])
+b = np.array([2,4,6,8,10,12,14])
+print("The first Array is ")
+print(a)
+print("The Second Array is ")
+print(b)
+c = a*b
+print("The Product Of The Array is ")
+print(c)
