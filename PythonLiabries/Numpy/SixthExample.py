ab308382699392c8cf8ffc2a8ceb7335f4f43b2e
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-01-02 00:44:33 +0530

    coded

--- PythonLiabries/Numpy/SixthExample.py
+++ PythonLiabries/Numpy/SixthExample.py
@@ -8,3 +8,18 @@ print(b)
 c = a*b
 print("The Product Of The Array is ")
 print(c)
+
+#After using Broadcasting technique
+num1 = np.array([
+    [1,2,3,4],
+    [2,4,5,6],
+    [10,20,39,3]
+])
+print("The First Numpy Array is ")
+print(num1)
+num2 = np.array([2,4,6,8])
+print("The Second Numpy Array is ")
+print(num2)
+sum = num2+num1
+print("The Sum of The Array is ")
+print(sum)
