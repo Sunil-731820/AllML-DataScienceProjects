4c15bf092dc26bab269e3e6f2d43ec225cdcf76d
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-02-15 00:41:02 +0530

    coded in Numpy

---
+++ PythonLiabries/Numpy/MatheMaticalfunctionExampleOfNumpyAgainJavatPoint.py
@@ -0,0 +1,7 @@
+import  numpy as np
+
+arr = np.array([0,30,60,90,120,150,180])
+print("The Array is ")
+print(arr)
+print("The Sin Values of The sin() is ")
+print(np.sin(arr))
--- PythonLiabries/Numpy/SecondExampleOfNumpyDatatypeAgainJavatPoint.py
+++ PythonLiabries/Numpy/SecondExampleOfNumpyDatatypeAgainJavatPoint.py
@@ -1,1 +1,4 @@
 import numpy as np
+
+# Numpy Datatype : Numpy Data types provides a Higher range of tHe Numeric data Type
+
