6b277410b079f78ac5e82d4783ebaa5503c33b12
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-02-15 23:43:00 +0530

    "Coded In numpy"

--- PythonLiabries/Numpy/NumpyMatheMaticalFunctions.py
+++ PythonLiabries/Numpy/NumpyMatheMaticalFunctions.py
@@ -27,3 +27,62 @@ print(arr11)
 print("The Matrix Of The Above Array is ")
 arr12 = np.array(np.mat('10 20; 30 40'),subok=True)
 print(arr12)
+
+
+# The use of The numpy.concatenate()
+x = numpy.array([[1,2],[3,4]])
+print("The First Array is ")
+print(x)
+y = numpy.array([[12,30]])
+print("the Second Array is ")
+print(y)
+print("after the use of The numpy.concatenate()")
+z = numpy.concatenate((x,y))
+print(z)
+
+# Concatnening the Two Array According column Wise and Rows Wise
+
+x1 = numpy.array([[1,2],[3,4]])
+y1 = numpy.array([[12,30]])
+print("the x1 is ")
+print(x1)
+print("The y1 is ")
+print(y1)
+print("After Concatening According to column Wise")
+print(numpy.concatenate((x1,y1),axis=0))
+print("After Concatening According to the Row Wise")
+# print(numpy.concatenate((x1,y1),axis=1))
+
+
+# The use of The numpy.append()
+
+firstArray = numpy.array([[10,20,30],[40,50,60],[70,80,90]])
+secondArray = numpy.array([[11,21,31],[42,52,62],[73,83,93]])
+print("The first Array is ")
+print(firstArray)
+print("the Second array is ")
+print(secondArray)
+print("after appending Both the Array is  ")
+appendArray = numpy.append(firstArray,secondArray)
+print(appendArray)
+
+# Appending the array according to axis Wise
+firstArray1 = numpy.array([[10,20,30],[40,50,60],[70,80,90]])
+secondArray1 = numpy.array([[11,21,31],[42,52,62],[73,83,93]])
+print("The First array is ")
+print(firstArray1)
+print("The Second Array is ")
+print(secondArray1)
+appendArray1 = numpy.append(firstArray1,secondArray1,axis=0)
+print("after The appending The array is ")
+print(appendArray1)
+rowWiseAppending = numpy.append(firstArray1,secondArray1,axis=1)
+print("after Appending The Rows Wise is ")
+print(rowWiseAppending)
+
+
+# The Use Of The
+
+
+
+
