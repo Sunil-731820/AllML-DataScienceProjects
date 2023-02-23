427d6a2d3069e801720c04a233dc5c7910dcc779
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-02-24 00:55:36 +0530

    coded

---
+++ PythonLiabries/Pandas/Pandas DataFrame/Pandas_DataFrame.query().py
@@ -0,0 +1,13 @@
+import  pandas as pd
+import numpy as np
+
+info = pd.DataFrame(
+    {
+        "X":range(1,6),
+        "Y":range(10,0,-2),
+        "Z Z": range(10,5,-1)
+    }
+)
+print("The info is ")
+print(info)
+print(info.query("X > Y"))
--- PythonLiabries/Pandas/Pandas DataFrame/Pandas_DataFrame_pivot_table().py
+++ PythonLiabries/Pandas/Pandas DataFrame/Pandas_DataFrame_pivot_table().py
@@ -3,6 +3,14 @@ import numpy as np
 
 info = pd.DataFrame(
     {
-        ""
+        "P":["Smith","John","William","Parker"],
+        "Q":["Python","C","Java","C++"],
+        "R":[19,24,22,25]
     }
+
 )
+print("The Info is ")
+print(info)
+table = pd.pivot_table(info,index=["P","Q"])
+print("The Table is ")
+print(table)
