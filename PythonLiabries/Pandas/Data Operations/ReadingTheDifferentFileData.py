5f062b8f5904e8ad9b657bfb6ee504f220e4ef92
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-02-26 00:58:23 +0530

    "Coded In numpy"

--- PythonLiabries/Pandas/Data Operations/ReadingTheDifferentFileData.py
+++ PythonLiabries/Pandas/Data Operations/ReadingTheDifferentFileData.py
@@ -3,15 +3,15 @@ import numpy as np
 import sqlite3
 # Reading The JSON data is
 
-# data = pd.read_json("Employee_JSON_Data.json")
-# print("The JSOn data is ")
-# print(data)
-#
+data = pd.read_json("EmployeeData.json")
+print("The JSOn data is ")
+print(data)
+
 
 # Reading The Data From THE dataBase
 # Creating the connections database and Pycharm
-con = sqlite3.connect("agentdatabase.db")
-df = pd.read_sql_query("select *from agent",con)
-print("The Data Frame is ")
-print(df)
-
+# con = sqlite3.connect("agentdatabase.db")
+# df = pd.read_sql_query("select *from agent",con)
+# print("The Data Frame is ")
+# print(df)
+#
