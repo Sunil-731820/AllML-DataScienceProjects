0615db4b8877396ed3e9524b812a18041d3c6b7c
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-03-02 13:08:05 +0530

    "Coded"

--- PythonLiabries/SciPy/Imagemanipulations.py
+++ PythonLiabries/SciPy/Imagemanipulations.py
@@ -1,12 +1,12 @@
 import imageio
 from imageio import imread, imsave
-import
+# import
 img = imread('C:\\Users\\sunil\\OneDrive\\Desktop\\Sunil Images\\sunilIImg1.jpg')
 print(img.shape)
 print(img.dtype)
 # Tinting The Image
 img_tint = img*[1,0.45,0.3]
-imsave('C:\\Users\\sunil\\OneDrive\\Desktop\\Sunil Images\\sunilIImg21.jpg')
+# imsave('C:\\Users\\sunil\\OneDrive\\Desktop\\Sunil Images\\sunilIImg21.jpg')
 # resising the Tinted imgage to be 300 x 300 pixels
 
-img_tint_resize = imr
+# img_tint_resize = imread()
