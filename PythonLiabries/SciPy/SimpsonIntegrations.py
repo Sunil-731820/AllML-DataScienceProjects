001ed082afb83e84cfbc2178fe055432184ee006
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-03-02 13:33:38 +0530

    "Coded"

---
+++ PythonLiabries/SciPy/SimpsonIntegrations.py
@@ -0,0 +1,12 @@
+import numpy as np
+def f1(x):
+   return x**2
+
+def f2(x):
+   return x**3
+
+x = np.array([1,3,4])
+y1 = f1(x)
+from scipy import integrate
+I1 = integrate.simpson(y1, x)
+print(I1)
