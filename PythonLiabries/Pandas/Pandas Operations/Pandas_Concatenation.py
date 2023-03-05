7af971984ecc85478a27aee5e6f8969bf0ac74b3
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-03-05 15:55:14 +0530

    code

--- PythonLiabries/Pandas/Pandas Operations/Pandas_Concatenation.py
+++ PythonLiabries/Pandas/Pandas Operations/Pandas_Concatenation.py
@@ -12,3 +12,27 @@ print("After Concateing The Data is ")
 concated_data = pd.concat([first_data,second_Data],ignore_index=True)
 print("The Data is ")
 print(concated_data)
+
+# Concatening The Series using append()
+
+first_Series = pd.Series(
+    {
+        "Name":["Smith","Perker","Allen","John","Parker"],
+        "Subject":["sub1","sub2","sub4","sub6","sub5"],
+        "Marks_Scored":[98,90,87,69,78]
+    }
+)
+
+second_Series = pd.Series(
+    {
+        "Name":["Billy","Brian","Bran","Bryce","betty"],
+        "Subject":["sub2","sub4","sub3","sub6","sub5"]
+        ,"Marks_scored":[89,80,79,97,88]
+    }
+)
+print("The First Data Series is")
+print(first_Series)
+print("The Second Data Series is")
+print(second_Series)
+print("After The Use of The append() for Concatenating The Series is ")
+print(first_Series.append(second_Series))
