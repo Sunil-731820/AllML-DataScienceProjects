223c43b3fec31ba88a3ecc5e8719f119e55460ae
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-05-23 23:35:30 +0530

    "Code is Done In Python "

--- AllMLDataSets/Test/first.py
+++ AllMLDataSets/Test/first.py
@@ -1,10 +1,14 @@
 
 def find_max(nums):
+    print("Finding The Maximum Numbers ")
     max_num = float("-inf")
     for num in nums:
         if (num>max_num):
             num = max_num
     return max_num
 
-max = find_max(120)
+# max = find_max(120)
+
+print("The maximum values is ")
 print(max)
+
